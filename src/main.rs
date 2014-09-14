e91891eb1cd74d853b67b0dab820670446d6509a
Max New <maxsnew@gmail.com>
2014-09-14 11:44:11 -0400

    Hello, window!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate graphics;
+extern crate piston;
+extern crate sdl2_game_window;
+extern crate opengl_graphics;
+
+use opengl_graphics::Gl;
+use sdl2_game_window::WindowSDL2;
+
 fn main() {
-    println!("Hello, world!")
+    let opengl = piston::shader_version::opengl::OpenGL_3_2;
+    let mut window = WindowSDL2::new(
+        opengl,
+        piston::WindowSettings::default());
+    let event_settings = piston::EventSettings {
+        updates_per_second:    120,
+        max_frames_per_second: 60
+    };
+    let ref mut gl = Gl::new(opengl);
+    for e in piston::EventIterator::new(&mut window, &event_settings) {};
 }
