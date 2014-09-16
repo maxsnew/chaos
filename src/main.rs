8bfb07338f54068efe89e7c79496bda83b03a3da
Max New <maxsnew@gmail.com>
2014-09-15 20:44:47 -0400

    Hello, black :/

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+extern crate num;
 extern crate opengl_graphics;
 extern crate piston;
 extern crate sdl2_game_window;
 
+use num::complex::Complex;
 use opengl_graphics::{Gl, Texture};
 use piston::graphics::{AddColor, AddImage, Context, Draw};
+use piston::image::Pixel;
 use piston::{Render, image};
+use piston::image::GenericImage;
 use sdl2_game_window::WindowSDL2;
 
 fn main() {
@@ -16,16 +20,43 @@ fn main() {
         ..piston::WindowSettings::default()
     };
     let mut window = WindowSDL2::new(opengl, window_settings);
-    let mut image = image::ImageBuf::new(w, h);
-    let mut texture = Texture::from_image(&image);
     let event_settings = piston::EventSettings {
         updates_per_second:    120,
         max_frames_per_second: 60
     };
     let ref mut gl = Gl::new(opengl);
+    let max_iterations = 256u16;
+    let imgx = 800;
+    let imgy = 800;
+    let scalex = 4.0 / imgx as f32;
+    let scaley = 4.0 / imgy as f32;
+    //Create a new ImgBuf with width: imgx and height: imgy
+    let mut imbuf: image::ImageBuf<image::Rgba<u8>> = image::ImageBuf::new(imgx, imgy);
+    for y in range(0, imgy) {
+        let cy = y as f32 * scaley - 2.0;
+        for x in range(0, imgx) {
+            let cx = x as f32 * scalex - 2.0;
+            let mut z = Complex::new(cx, cy);
+            let c = Complex::new(-0.4, 0.6);
+            let mut i = 0;
+            for t in range(0, max_iterations) {
+                if z.norm() > 2.0 {
+                    break
+                }
+                z = z * z + c;
+                i = t;
+            }
+            // Create an 8bit pixel of type Luma and value i
+            let pixel = image::Luma(i as u8).to_rgba();
+            // Put a pixel in the image at coordinates x and y
+            imbuf.put_pixel(x, y, pixel);
+        }
+    }
+    let mut texture = Texture::from_image(&imbuf);
     for e in piston::EventIterator::new(&mut window, &event_settings) {
         match e {
             Render(args) => {
+                
                 gl.viewport(0, 0, args.width as i32, args.height as i32);
                 let c = Context::abs(args.width as f64, args.height as f64);
                 c.rgb(1.0, 1.0, 1.0).draw(gl);
